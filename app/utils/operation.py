f77e9106f184cf9c13edcfdb10b677d255284893
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2024-05-06 23:59:45 +0200

    [UPDATE] send message 15 minutes before

--- app/utils/operation.py
+++ app/utils/operation.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from uuid import uuid4
 from .schedule import schedule, cancel_schedule
 from .parse import parse_fields
 from app.utils.logging import logger
+from app.settings import settings
 from ..exceptions import InvalidOptionsCount
 from ..schemas import DiscordRequestData, OptionName, DATE_SEPARATOR, TIME_SEPARATOR
 
@@ -23,18 +24,21 @@ async def plan_meeting(data: DiscordRequestData) -> str:
         elif option.name == OptionName.DESCRIPTION:
             description = option.value
 
-    day, month = parse_fields(2, DATE_SEPARATOR, date)
-    hour, minute = parse_fields(2, TIME_SEPARATOR, time)
+    day, month = parse_fields(2, DATE_SEPARATOR, date, int)
+    hour, minute = parse_fields(2, TIME_SEPARATOR, time, int)
     year = datetime.now().year
 
+    current_date = datetime(year, month, day, hour, minute)
+    current_date -= timedelta(minutes=settings.MINUTES_REMINDER)
+
     await schedule(
-        name,
-        description,
-        year=year,
-        month=month,
-        day=day,
-        hour=hour,
-        minute=minute
+        name=name,
+        description=description,
+        year=current_date.year,
+        month=current_date.month,
+        day=current_date.day,
+        hour=current_date.hour,
+        minute=current_date.minute
     )
 
     logger.info(f"Meeting {name} planned for {date} at {time} - {description}")
